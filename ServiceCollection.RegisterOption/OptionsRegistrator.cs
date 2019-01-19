0baa444080b7bb7ece52c5bae2f9c5c5b1d8f5d0
Oleg <oleg.bryuhanov@hotmail.com>
2019-01-19 13:54:21 +0300

    files of project

---
+++ ServiceCollection.RegisterOption/OptionsRegistrator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ServiceCollection.RegisterOption
+{
+    public static class OptionsRegistrator
+    {
+        private const string DefaultOptionsPostfix = "Options";
+        
+        public static IServiceCollection RegisterOptions<TOptions>(this IServiceCollection serviceCollection,
+            IConfiguration configuration, string sectionName = null) where TOptions: class
+        {
+            if (string.IsNullOrEmpty(sectionName))
+            {
+                var genericClassOptionsName = nameof(TOptions);
+
+                if (genericClassOptionsName.EndsWith(DefaultOptionsPostfix))
+                {
+                    genericClassOptionsName =
+                        genericClassOptionsName.Remove(genericClassOptionsName.Length - DefaultOptionsPostfix.Length);
+                }
+
+                sectionName = genericClassOptionsName;
+            }
+            
+            return serviceCollection.Configure<TOptions>(configuration.GetSection(sectionName));
+        }
+    }
+}
